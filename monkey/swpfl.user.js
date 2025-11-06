8c45b907888e08650472c81697bc583ded1ceea3
J.L. De Guzman <118164738+jayxdcode@users.noreply.github.com>
2025-11-06 08:20:43 +0800

    updated monkey/swpfl.user.js to v2.9.0

--- monkey/swpfl.user.js
+++ monkey/swpfl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [BETA] Spotify Web Player Floating Lyrics
 // @namespace    http://tampermonkey.net/
-// @version      2.8.7
+// @version      2.9.0
 // @description  Synced lyrics with translation/romanization resizable/draggable panel, themed, opacity control. Translations are provided by Gemini 2.0 Flash and 1.5 Flash via the Google AI Studio API (Accessed via a remote server).
 // @author       jayxdcode
 // @match        https://open.spotify.com/*
@@ -26,7 +26,7 @@
     let got = false;
 
     // -- begin --
-    const SWPFL_VERSION = '2.8.7';
+    const SWPFL_VERSION = '2.9.0';
     const SWPFL_USER_AGENT = `SWPFL (user.js release) v${SWPFL_VERSION} (https://github.com/jayxdcode/swpfl)`;
 
     const LRCLIB_HEADERS = {
